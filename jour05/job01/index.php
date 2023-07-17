407e3279812ff0252049074aa904dd8d338dba20
Joshua Godart <joshua.godart@laplateforme.io>
2023-07-17 12:38:02 +0200

    ajout jour 5 et 6

---
+++ jour05/job01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="post">
+        <input type="text" name="nom" placeholder="Nom"><br>
+        <input type="text" name="prenom" placeholder="Prénom"><br>
+        <input type="text" name="email" placeholder="Email"><br>
+        <input type="password" name="mdp" placeholder="Mot de passe"><br>
+        <input type="submit" value="Connexion">
+    </form>
+</body>
+</html>
